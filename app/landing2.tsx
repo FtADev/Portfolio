5ea2d9aa781b0257ee7ca1827cffb3a3deca6ece
fatemeh <akhlaghi.fatemeh@gmail.com>
2025-02-20 20:53:04 +0330

    Minor

--- app/landing2.tsx
+++ app/landing2.tsx
@@ -1,26 +1,25 @@
 import React from "react";
 import { StarsBackground } from "./components/ui/stars-background";
 import { ShootingStars } from "./components/ui/shooting-stars";
-import { TypewriterEffectSmooth } from "./components/ui/typewriter-effect";
 import { FlipWords } from "./components/ui/flip-words";
 
 const LandingPage2 = () => {
-  const words = ["Front-End", "Back-End", "Full-Stack"];
+  const words = ["Back-End", "Front-End", "Full-Stack"];
 
   return (
     <div className="flex w-screen h-screen bg-slate-950">
       <StarsBackground />
       <ShootingStars />
       <div className="flex flex-col justify-center items-center text-center text-white mx-auto my-auto gap-20">
-          <span className="text-3xl">Hello! It's me!</span>
-          <span className="text-8xl italic bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
-            {"Fatemeh Akhlaghi".toUpperCase()}
-            </span>
-            <div className="text-4xl mx-auto font-normal text-neutral-600 dark:text-neutral-400">
-              As a
-              <FlipWords words={words} />
-              Developer
-            </div>
+        <span className="text-3xl">Hello! It's me!</span>
+        <span className="text-8xl italic bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
+          {"Fatemeh Akhlaghi".toUpperCase()}
+        </span>
+        <div className="text-4xl mx-auto font-normal text-neutral-600 dark:text-neutral-400">
+          As a
+          <FlipWords words={words} />
+          Developer
+        </div>
       </div>
     </div>
   );
