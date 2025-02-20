28b8a5f5c53c92df1da9c6580eb77fc15ee92724
fatemeh <akhlaghi.fatemeh@gmail.com>
2025-02-20 21:09:33 +0330

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import { LandingPage } from "./landing";
 import LandingPage2 from "./landing2";
+import SolarSystem from "./solar-system";
 
 export default function Home() {
   return (
     <div className="flex bg-white w-full h-full">
       {/* <LandingPage /> */}
       <LandingPage2 />
+      {/* <SolarSystem /> */}
     </div>
   );
 }
