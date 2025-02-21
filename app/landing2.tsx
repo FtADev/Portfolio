b69d5d145ad85ecd418411e4e10910bf4498a174
fatemeh <akhlaghi.fatemeh@gmail.com>
2025-02-21 11:17:23 +0330

    minor

--- app/landing2.tsx
+++ app/landing2.tsx
@@ -11,7 +11,7 @@ const LandingPage2 = () => {
       <StarsBackground />
       <ShootingStars />
       <div className="flex flex-col justify-center items-center text-center text-white mx-auto my-auto gap-20">
-        <span className="text-3xl">Hello! It's me!</span>
+        <span className="text-4xl mx-auto font-normal text-neutral-600 dark:text-neutral-400">Hello! It's me!</span>
         <span className="text-8xl italic bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
           {"Fatemeh Akhlaghi".toUpperCase()}
         </span>
