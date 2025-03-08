76a379ae88c66ba83a27a7a0743134054ea66a92
Fatemeh Akhlaghi <akhlaghi.fatemeh@gmail.com>
2025-03-08 15:14:14 +0330

    Update footer.tsx

--- app/sections/footer.tsx
+++ app/sections/footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 const FooterSection = () => {
   return (
     <section className="relative w-ful">
-      <div className="absolute top-0 bg-bgPrimary h-[100px] w-full rounded-b-[100%] z-50"></div>
+      <div className="absolute top-0 bg-white h-[100px] w-full rounded-b-[100%] z-50"></div>
       <div className="absolute top-[50px] h-[200px] w-full  bg-gradient-to-b bg-[#FFD6E8] z-0">
         <div className="mt-[130px] text-center text-sm text-white ">
           © All Right Reserved By{" "}
