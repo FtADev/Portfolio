cf47735463ef0f3133d0f7fd946f3a0d85f154fa
fatemeh <akhlaghi.fatemeh@gmail.com>
2025-03-21 18:28:56 +0330

    Add animation to about page

--- app/sections/about.tsx
+++ app/sections/about.tsx
@@ -4,6 +4,7 @@ import Collapsable from "../components/collapsable";
 import ImageWithBG from "../components/image-bg";
 import LiquidShape from "../components/liquid-shape";
 import Link from "next/link";
+import { motion } from "framer-motion";
 
 const AboutSection = () => {
   const shapeColor = "#FFD6E8";
@@ -110,6 +111,70 @@ const AboutSection = () => {
     },
   ];
 
+  const titleVarient = {
+    hidden: { opacity: 0, y: 100 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        ease: "easeOut",
+        duration: 1,
+      },
+    },
+  };
+
+  const imageVarient = {
+    hidden: { opacity: 0, y: 100 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        ease: "easeOut",
+        duration: 1,
+        delay: 0.5,
+      },
+    },
+  };
+
+  const descVarient = {
+    hidden: { opacity: 0, y: 100 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        ease: "easeOut",
+        duration: 1,
+        delay: 0.5,
+      },
+    },
+  };
+
+  const expVarient = {
+    hidden: { opacity: 0, y: 100 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        ease: "easeOut",
+        duration: 1,
+        delay: 1,
+      },
+    },
+  };
+
+  const cvVarient = {
+    hidden: { opacity: 0, y: 100 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        ease: "easeOut",
+        duration: 1,
+        delay: 1,
+      },
+    },
+  };
+
   return (
     <section
       id="about"
@@ -121,34 +186,57 @@ const AboutSection = () => {
       />
 
       <div className="grid grid-rows-4 grid-cols-1 md:grid-cols-2 w-full h-full gap-4">
-
         {/* Image */}
-        <div className="row-span-3 order-2 md:order-1 md:pt-8">
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          variants={imageVarient}
+          viewport={{ once: true }}
+          className="row-span-3 order-2 md:order-1 md:pt-8"
+        >
           <ImageWithBG
             color={shapeColor}
             imageSrc="/about.svg"
             size="md:w-[500px] md:h-[500px] w-[300px] h-[300px]"
           />
-        </div>
+        </motion.div>
 
         {/* TITLE */}
-        <div className="relative row-span-1 order-1 md:order-2 md:text-start z-10">
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          variants={titleVarient}
+          viewport={{ once: true }}
+          className="relative row-span-1 order-1 md:order-2 md:text-start z-10"
+        >
           <span className="absolute bottom-0 right-0 left-0 text-bgPink font-bold [font-size:_clamp(2rem,4vw,3.5rem)]">
             About Me
           </span>
-        </div>
+        </motion.div>
 
         {/* Description */}
         <div className="row-span-3 order-3 z-10">
           <div className="flex flex-col md:gap-8 gap-3 ">
-            <p className="text-justify font-normal [font-size:_clamp(0.5rem,3vw,1.125rem)]">
+            <motion.p
+              initial="hidden"
+              whileInView="visible"
+              variants={descVarient}
+              viewport={{ once: true }}
+              className="text-justify font-normal [font-size:_clamp(0.5rem,3vw,1.125rem)]"
+            >
               I'm experienced FullStack Developer with 5+ years of expertise in
               Frontend and Backend Technology. I have more than 1 year of
               experience as a Backend Developer of Java. I am now seeking to
               expand my knowledge in Backend Development. Language is not a
               problem for me! I can switch easily between other languages!
-            </p>
-            <div className="flex flex-col">
+            </motion.p>
+            <motion.div
+              initial="hidden"
+              whileInView="visible"
+              variants={expVarient}
+              viewport={{ once: true }}
+              className="flex flex-col"
+            >
               {experiences.map((item, index) => (
                 <Collapsable
                   key={index}
@@ -162,19 +250,25 @@ const AboutSection = () => {
                   onClick={() => onExpClick(index)}
                 />
               ))}
-            </div>
+            </motion.div>
           </div>
         </div>
 
         {/* Button */}
-        <div className="row-span-1 order-4 mt-4">
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          variants={cvVarient}
+          viewport={{ once: true }}
+          className="row-span-1 order-4 mt-4"
+        >
           <Link
             href="https://drive.google.com/file/d/19p_mKaXVBX-SwpUd-ZjX1_S_e3Vhe-QP/view?usp=sharing"
             className=" bg-bgPink rounded-xl py-2 px-5 text-white font-bold mx-auto text-lg shadow-md"
           >
             View My CV
           </Link>
-        </div>
+        </motion.div>
       </div>
     </section>
   );
