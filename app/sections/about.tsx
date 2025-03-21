5d732891c00b7971454e879fc8a8178bf7af32a5
fatemeh <akhlaghi.fatemeh@gmail.com>
2025-03-21 16:21:32 +0330

    Add grid to about page

--- app/sections/about.tsx
+++ app/sections/about.tsx
@@ -113,7 +113,7 @@ const AboutSection = () => {
   return (
     <section
       id="about"
-      className="relative w-full h-full flex min-h-screen items-center justify-center mx-auto max-w-7xl px-4 py-12 gap-10"
+      className="relative w-full h-full flex min-h-screen items-center justify-center mx-auto max-w-7xl px-4 py-12 gap-10 text-center"
     >
       <LiquidShape
         className="hidden md:block"
@@ -121,25 +121,27 @@ const AboutSection = () => {
         position="-top-[100px] -right-[300px]"
       />
 
-      <div className="flex flex-col w-full gap-8 ">
-        <div className="flex md:flex-row flex-col gap-8">
-          <div className="flex flex-col gap-1 h-full item-start">
-            <ImageWithBG
-              color={shapeColor}
-              imageSrc="/about.svg"
-              size="md:w-[500px] md:h-[500px] w-[300px] h-[300px]"
-            />
-            <Link
-              href="https://drive.google.com/file/d/19p_mKaXVBX-SwpUd-ZjX1_S_e3Vhe-QP/view?usp=sharing"
-              className="hidden md:block bg-bgPink rounded-xl py-2 px-5 text-white font-bold mx-auto text-lg shadow-md"
-            >
-              View My CV
-            </Link>
-          </div>
-          <div className="flex flex-col md:w-2/3 md:gap-8 gap-3 z-10">
-            <span className="text-bgPink font-bold [font-size:_clamp(2rem,4vw,3.5rem)] z-10">
-              About Me
-            </span>
+      <div className="grid grid-rows-4 grid-cols-1 md:grid-cols-2 w-full h-full gap-8">
+        
+        {/* Image */}
+        <div className="row-span-3 order-2 md:order-1">
+          <ImageWithBG
+            color={shapeColor}
+            imageSrc="/about.svg"
+            size="md:w-[500px] md:h-[500px] w-[300px] h-[300px]"
+          />
+        </div>
+
+        {/* TITLE */}
+        <div className="row-span-1 order-1 md:order-2 md:text-start z-10">
+          <span className=" text-bgPink font-bold [font-size:_clamp(2rem,4vw,3.5rem)]">
+            About Me
+          </span>
+        </div>
+
+        {/* Description */}
+        <div className="row-span-4 order-3 z-10">
+          <div className="flex flex-col md:gap-8 gap-3 ">
             <p className="text-justify font-normal [font-size:_clamp(0.5rem,3vw,1.125rem)]">
               I'm experienced FullStack Developer with 5+ years of expertise in
               Frontend and Backend Technology. I have more than 1 year of
@@ -164,12 +166,16 @@ const AboutSection = () => {
             </div>
           </div>
         </div>
-        <Link
-          href="https://drive.google.com/file/d/19p_mKaXVBX-SwpUd-ZjX1_S_e3Vhe-QP/view?usp=sharing"
-          className="block md:hidden bg-bgPink rounded-xl py-2 px-5 text-white font-bold mx-auto shadow-md text-sm"
-        >
-          View My CV
-        </Link>
+
+        {/* Button */}
+        <div className="row-span-1 order-4">
+          <Link
+            href="https://drive.google.com/file/d/19p_mKaXVBX-SwpUd-ZjX1_S_e3Vhe-QP/view?usp=sharing"
+            className=" bg-bgPink rounded-xl py-2 px-5 text-white font-bold mx-auto text-lg shadow-md"
+          >
+            View My CV
+          </Link>
+        </div>
       </div>
     </section>
   );
