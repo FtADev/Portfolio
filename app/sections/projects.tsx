5efd0567079c3a6f97745f84d630ab62d54910d2
Fatemeh Akhlaghi <akhlaghi.fatemeh@gmail.com>
2025-03-29 11:47:11 +0330

    Update projects.tsx

--- app/sections/projects.tsx
+++ app/sections/projects.tsx
@@ -7,7 +7,6 @@ import LiquidShape from "../components/liquid-shape";
 import SelectedButton from "../components/selected-button";
 import Link from "next/link";
 import ProjectCard from "../components/project-card";
-import { motion } from "framer-motion";
 
 const ProjectSection = () => {
   const shapeColor = "#FFD6E8";
@@ -30,80 +29,65 @@ const ProjectSection = () => {
   const projects = [
     {
       title: "Bill Services",
-      src: "/bill.png",
+      src: "/bill.svg",
       type: 1,
       link: "https://bankmellat.ir/electronic_payment_of_bill.aspx",
     },
     {
       title: "Financial Capability",
-      src: "/tamakon3.png",
+      src: "/tamakon.svg",
       type: 1,
       link: "https://bankmellat.ir/deposit_account.aspx",
     },
     {
       title: "Namacheque Service",
-      src: "/cheque.jpg",
+      src: "/credit.svg",
       type: 1,
       link: "https://bankmellat.ir/nama_check.aspx",
     },
     {
       title: "Financial Inquiry",
-      src: "/tamakon2.png",
+      src: "/document.svg",
       type: 2,
       link: "",
     },
     {
       title: "Coffee Shop",
-      src: "/blog-img3.jpg",
+      src: "/coffee.svg",
       type: 2,
       link: "",
     },
     {
       title: "Ecommerce",
-      src: "/ecommerce.png",
+      src: "/ecommerce.svg",
       type: 2,
-      link: "https://ecommerce-db-two.vercel.app/",
+      link: "",
     },
     {
       title: "Payatam",
-      src: "/payatam.png",
+      src: "/team.svg",
       type: 3,
       link: "/project/payatam",
     },
     {
       title: "Timeset",
-      src: "/timeset.png",
+      src: "/sport.svg",
       type: 3,
       link: "/project/timeset",
     },
     {
       title: "Pet Customer",
-      src: "/pet-customer.png",
+      src: "/pet.svg",
       type: 3,
       link: "/project/pet-customer",
     },
   ];
 
-  const titleVarient = {
-    hidden: { opacity: 0, y: 100 },
-    visible: {
-      opacity: 1,
-      y: 0,
-      transition: {
-        ease: "easeOut",
-        duration: 1,
-      },
-    },
-  };
-
   const [filterProjects, setProjects] = useState(projects);
   const [selectedIndex, setSelectedIndex] = useState(0);
 
   return (
-    <section
-      id="projects"
-      className="relative w-full h-full flex min-h-screen items-center justify-center mx-auto max-w-7xl px-4 py-16 gap-10"
-    >
+    <section id="projects" className="relative w-full h-full flex min-h-screen items-center justify-center mx-auto max-w-7xl px-4 py-28 gap-10">
       <LiquidShape
         color={shapeColor}
         position="-top-[100px] -left-[300px]"
@@ -112,18 +96,8 @@ const ProjectSection = () => {
       />
 
       <div className="flex flex-col w-full gap-8 justify-center items-center z-10">
-        <motion.div
-          initial="hidden"
-          whileInView="visible"
-          variants={titleVarient}
-          viewport={{ once: true }}
-          className="z-10"
-        >
-          <span className="text-bgPink font-bold [font-size:_clamp(2rem,4vw,3.5rem)]">
-            My Projects
-          </span>
-        </motion.div>
-        <div className="flex lg:gap-7 gap-1 mt-5">
+        <span className="text-bgPink font-bold [font-size:_clamp(2rem,4vw,3.5rem)]">My Projects</span>
+        <div className="flex md:gap-7 gap-1 mt-5">
           {projectTypes.map((type, index) => (
             <SelectedButton
               key={index}
@@ -143,16 +117,9 @@ const ProjectSection = () => {
             />
           ))}
         </div>
-        <div className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-5 max-w-5xl mx-auto w-full justify-center z-10 px-4">
+        <div className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-5 max-w-5xl mx-auto md:px-8 w-full justify-center z-10">
           {filterProjects.map((item, index) => (
-            <ProjectCard
-              key={index}
-              index={index}
-              title={item.title}
-              link={item.link}
-              src={item.src}
-              description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, quaerat!"
-            />
+            <ProjectCard key={index} title={item.title} link={item.link} src={item.src} description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, quaerat!"/>
           ))}
         </div>
       </div>
