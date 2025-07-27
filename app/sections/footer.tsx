6e39c8495d4ec1ed93834eb41cf7975c5f0582bd
Fatemeh Akhlaghi <akhlaghi.fatemeh@gmail.com>
2025-07-27 08:43:02 +0330

    Update footer.tsx

--- app/sections/footer.tsx
+++ app/sections/footer.tsx
@@ -4,7 +4,7 @@ const FooterSection = () => {
     <section className="relative w-full bg-[#FFD6E8]">
       <div className="absolute top-0 bg-white h-[100px] w-full rounded-b-[100%] z-50"></div>
       <div className="absolute top-[50px] h-[200px] w-full  bg-gradient-to-b bg-[#FFD6E8] z-0">
-        <div className="mt-[130px] text-center text-sm text-white [font-size:_clamp(0.8rem,1vw,2rem)]">
+        <div className="mt-[130px] text-center text-sm text-bgPink [font-size:_clamp(0.8rem,1vw,2rem)]">
           © All Rights Reserved By{" "}
           <span className="font-bold">Fatemeh Akhlaghi</span>
         </div>
