5e45eb4befac896c244d483835020e6ea88cefe3
Fatemeh Akhlaghi <akhlaghi.fatemeh@gmail.com>
2025-07-30 08:08:04 +0330

    Update network-cv.tsx

--- app/components/network-cv.tsx
+++ app/components/network-cv.tsx
@@ -3,6 +3,7 @@ import React, { ReactNode } from "react";
 import { Github, Linkedin, Mail, Send } from "lucide-react";
 import Link from "next/link";
 import { motion } from "framer-motion";
+import { Variants, Easing } from "framer-motion";
 
 interface NetworkProps {
   index: number;
@@ -11,13 +12,13 @@ interface NetworkProps {
 }
 
 const NetworkCV = ({ index, url, icon }: NetworkProps) => {
-  const varient = {
+  const varient: Variants = {
     hidden: { opacity: 0, x: 100 },
     visible: {
       opacity: 1,
       x: 0,
       transition: {
-        ease: "easeOut",
+        ease: [0.25, 0.1, 0.25, 1],
         duration: 0.75,
         delay: 0.5 * index + 0.7,
       },
@@ -35,6 +36,7 @@ const NetworkCV = ({ index, url, icon }: NetworkProps) => {
       <Link
         href={url}
         className="text-bgPink hover:scale-125 transition-all duration-150"
+        target="_blank"
       >
         {icon}
       </Link>
